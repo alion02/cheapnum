532a88c870bfeb5cdd4bedd9f13397cbb01ededb
alion02 <alojczek@gmail.com>
2024-05-26 04:43:00 +0200

    Rewrite part 1

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::{
-    collections::{btree_map::Entry, BTreeMap},
-    fmt,
-};
+use std::{collections::BTreeMap, fmt};
 
 use anyhow::*;
+use bitvec::vec::BitVec;
 
+type Cost = u32;
 type N = i32;
 
 enum Op {
@@ -12,7 +11,7 @@ enum Op {
     Mul,
     Sub,
     Div,
-    Exp,
+    Pow,
 }
 
 impl fmt::Display for Op {
@@ -22,120 +21,119 @@ impl fmt::Display for Op {
             Op::Mul => "*",
             Op::Sub => "-",
             Op::Div => "/",
-            Op::Exp => "^",
+            Op::Pow => "^",
         })
     }
 }
 
 fn main() -> Result<()> {
+    #[inline]
+    fn idx(n: N) -> usize {
+        n as u32 as usize
+    }
+
     let mut args = std::env::args();
 
     let target: N = args.nth(1).context("missing target number")?.parse()?;
 
-    let mut numbers = BTreeMap::<u32, Vec<_>>::new();
-    let mut costs = args
-        .map(|n| {
-            let (n, cost) = if let Some((n, cost)) = n.split_once('=') {
-                (n, cost.parse().context("bad number cost")?)
-            } else {
-                (&n[..], 1)
-            };
+    let mut seen = BitVec::<u32>::repeat(false, 1 << 32).into_boxed_bitslice();
+    let mut numbers = BTreeMap::<Cost, Vec<N>>::new();
+    for arg in args {
+        let (n, cost) = if let Some((n, cost)) = arg.split_once('=') {
+            (n, cost.parse().context("bad number cost")?)
+        } else {
+            (&arg[..], 1)
+        };
 
-            ensure!(cost > 0, "bad number cost");
+        ensure!(cost > 0, "bad number cost");
 
-            let n = n.parse().context("bad number")?;
-            numbers.entry(cost).or_default().push(n);
+        let n = n.parse().context("bad number")?;
 
-            Ok((n, cost))
-        })
-        .collect::<Result<BTreeMap<N, u32>>>()?;
-
-    println!("initial numbers:");
-
-    costs
-        .iter()
-        .for_each(|(n, cost)| println!("  {n} (cost {cost})"));
-
-    let mut operations = BTreeMap::new();
+        numbers.entry(cost).or_default().push(n);
+        seen.set(idx(n), true);
+    }
 
     for new_cost in 1.. {
-        println!(
-            "searching for cost {new_cost} ({} total numbers reached)",
-            costs.len(),
-        );
+        let mut new_numbers = numbers.remove(&new_cost).unwrap_or_default();
 
-        let new_numbers: BTreeMap<N, (N, N, Op)> = (1..new_cost + 1)
-            .flat_map(|i| {
-                let j = new_cost - i;
+        println!("searching for cost {new_cost}");
 
-                let numbers = &numbers;
-                numbers
-                    .get(&i)
-                    .into_iter()
-                    .flat_map(|vec| vec.iter())
-                    .flat_map(move |&lhs| {
-                        numbers
-                            .get(&j)
-                            .into_iter()
-                            .flat_map(|vec| vec.iter())
-                            .flat_map(move |&rhs| {
-                                [
-                                    (lhs.saturating_add(rhs), (lhs, rhs, Op::Add)),
-                                    (lhs.saturating_mul(rhs), (lhs, rhs, Op::Mul)),
-                                    (lhs.saturating_sub(rhs), (lhs, rhs, Op::Sub)),
-                                    (
-                                        if rhs == 0 {
-                                            0
-                                        } else {
-                                            let res = lhs.saturating_div(rhs);
-                                            if res.saturating_mul(rhs) != lhs {
-                                                0
-                                            } else {
-                                                res
-                                            }
-                                        },
-                                        (lhs, rhs, Op::Div),
-                                    ),
-                                    (
-                                        if rhs < 0 {
-                                            0
-                                        } else {
-                                            lhs.saturating_pow(rhs as _)
-                                        },
-                                        (lhs, rhs, Op::Exp),
-                                    ),
-                                ]
-                            })
-                    })
-            })
-            .collect();
+        for i in 1..new_cost / 2 + 1 {
+            let j = new_cost - i;
 
-        let curr_cost = numbers.entry(new_cost).or_default();
-        new_numbers.into_iter().for_each(|(n, op)| {
-            if let Entry::Vacant(v) = costs.entry(n) {
-                v.insert(new_cost);
-                operations.insert(n, op);
-                curr_cost.push(n);
-            }
-        });
+            let a_slice = numbers.get(&i).unwrap().as_slice();
+            let b_slice = numbers.get(&j).unwrap().as_slice();
 
-        if let Some(cost) = costs.get(&target) {
-            fn print_tree(operations: &BTreeMap<N, (N, N, Op)>, n: &N, depth: usize) {
-                if let Some((lhs, rhs, op)) = operations.get(n) {
-                    println!("{}{lhs} {op} {rhs}", "  ".repeat(depth));
-                    print_tree(operations, lhs, depth + 1);
-                    if lhs != rhs {
-                        print_tree(operations, rhs, depth + 1);
+            for &a in a_slice {
+                for &b in b_slice {
+                    #[inline]
+                    fn div(a: N, b: N) -> N {
+                        if b == 0 {
+                            0
+                        } else {
+                            let res = a.saturating_div(b);
+                            if res.saturating_mul(b) != a {
+                                0
+                            } else {
+                                res
+                            }
+                        }
+                    }
+
+                    #[inline]
+                    fn pow(a: N, b: N) -> N {
+                        if b < 0 {
+                            0
+                        } else {
+                            a.saturating_pow(b as _)
+                        }
+                    }
+
+                    for n in [
+                        a.saturating_add(b),
+                        a.saturating_mul(b),
+                        a.saturating_sub(b),
+                        b.saturating_sub(a),
+                        div(a, b),
+                        div(b, a),
+                        pow(a, b),
+                        pow(b, a),
+                    ] {
+                        let seen = unsafe { seen.get_unchecked_mut(idx(n)) };
+                        if !*seen {
+                            new_numbers.push(n);
+                            seen.commit(true);
+                        }
                     }
                 }
             }
-
-            println!("found target number {target} (total cost {cost}):");
-            print_tree(&operations, &target, 1);
-
-            return Ok(());
         }
+
+        if seen[idx(target)] {
+            println!("found target number {target} (total cost {new_cost})");
+
+            break;
+        }
+
+        numbers.insert(new_cost, new_numbers);
+
+        // if let Some(cost) = costs.get(&target) {
+        //     fn print_tree(operations: &BTreeMap<N, (N, N, Op)>, n: &N, depth: usize) {
+        //         if let Some((lhs, rhs, op)) = operations.get(n) {
+        //             println!("{}{lhs} {op} {rhs}", "  ".repeat(depth));
+        //             print_tree(operations, lhs, depth + 1);
+        //             if lhs != rhs {
+        //                 print_tree(operations, rhs, depth + 1);
+        //             }
+        //         }
+        //     }
+
+        //     println!("found target number {target} (total cost {cost}):");
+        //     print_tree(&operations, &target, 1);
+
+        //     return Ok(());
+        // }
     }
 
-    unreachable!()
+    Ok(())
 }
